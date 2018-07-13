d99ba61791eece68accec3803fc0a51b00c4ef19
arthurchan35 <arthurchan35@gmail.com>
2018-07-13 15:31:10 +0800

    LPS-83223 same reason

---
+++ modules/apps/message-boards/message-boards-web/src/main/java/com/liferay/message/boards/web/internal/exportimport/data/handler/MBPortletDataHandler.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.web.internal.exportimport.data.handler;
+
+import com.liferay.exportimport.kernel.lar.DataLevel;
+import com.liferay.exportimport.kernel.lar.PortletDataHandler;
+import com.liferay.message.boards.constants.MBPortletKeys;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author arthurchan35
+ */
+@Component(
+	property = "javax.portlet.name=" + MBPortletKeys.MESSAGE_BOARDS,
+	service = PortletDataHandler.class
+)
+public class MBPortletDataHandler extends MBAdminPortletDataHandler {
+
+	@Activate
+	protected void activate() {
+		setDataLevel(DataLevel.PORTLET_INSTANCE);
+		super.activate();
+	}
+
+}
