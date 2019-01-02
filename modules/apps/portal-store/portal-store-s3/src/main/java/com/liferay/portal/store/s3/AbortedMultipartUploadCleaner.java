4817592cbebe3251ab497e6506c67bf3e81a0939
hai.yu <8219754@liferay.com>
2019-01-02 12:30:40 +0800

    LPS-88710 Invert service dependency to avoid calling SchedulerEngineHelper.register(): refactor AbortedMultipartUploaderCleaner to register its listener as SchedulerEventMessageListener service

--- modules/apps/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/AbortedMultipartUploadCleaner.java
+++ modules/apps/portal-store/portal-store-s3/src/main/java/com/liferay/portal/store/s3/AbortedMultipartUploadCleaner.java
@@ -22,12 +22,13 @@ import com.liferay.portal.kernel.messaging.BaseMessageListener;
 import com.liferay.portal.kernel.messaging.DestinationNames;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
 import com.liferay.portal.kernel.scheduler.SchedulerEntry;
 import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
 import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.messaging.SchedulerEventMessageListener;
+import com.liferay.portal.kernel.util.HashMapDictionary;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -35,11 +36,13 @@ import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 import java.util.Date;
+import java.util.Dictionary;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -65,7 +68,7 @@ public class AbortedMultipartUploadCleaner {
 					"(&(", Constants.OBJECTCLASS, "=", Store.class.getName(),
 					")(store.type=", S3Store.class.getName(), "))")),
 			new S3StoreServiceTrackerCustomizer(
-				bundleContext, _schedulerEngineHelper, _triggerFactory));
+				bundleContext, _triggerFactory));
 	}
 
 	@Deactivate
@@ -73,16 +76,18 @@ public class AbortedMultipartUploadCleaner {
 		_serviceTracker.close();
 	}
 
-	@Reference(unbind = "-")
-	private SchedulerEngineHelper _schedulerEngineHelper;
-
 	private ServiceTracker<S3Store, MessageListener> _serviceTracker;
 
 	@Reference(unbind = "-")
 	private TriggerFactory _triggerFactory;
 
 	private static class AbortedMultipartUploadMessageListener
-		extends BaseMessageListener {
+		extends BaseMessageListener implements SchedulerEventMessageListener {
+
+		@Override
+		public SchedulerEntry getSchedulerEntry() {
+			return _schedulerEntry;
+		}
 
 		@Override
 		protected void doReceive(Message message) throws Exception {
@@ -92,8 +97,11 @@ public class AbortedMultipartUploadCleaner {
 				_s3Store.getBucketName(), _computeStartDate());
 		}
 
-		private AbortedMultipartUploadMessageListener(S3Store s3Store) {
+		private AbortedMultipartUploadMessageListener(
+			S3Store s3Store, SchedulerEntry schedulerEntry) {
+
 			_s3Store = s3Store;
+			_schedulerEntry = schedulerEntry;
 		}
 
 		private Date _computeStartDate() {
@@ -112,6 +120,7 @@ public class AbortedMultipartUploadCleaner {
 		}
 
 		private final S3Store _s3Store;
+		private final SchedulerEntry _schedulerEntry;
 
 	}
 
@@ -121,10 +130,6 @@ public class AbortedMultipartUploadCleaner {
 		@Override
 		public MessageListener addingService(
 			ServiceReference<S3Store> serviceReference) {
-
-			MessageListener messageListener =
-				new AbortedMultipartUploadMessageListener(
-					_bundleContext.getService(serviceReference));
 
 			Class<?> clazz = getClass();
 
@@ -136,11 +141,21 @@ public class AbortedMultipartUploadCleaner {
 			SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
 				className, trigger);
 
-			_schedulerEngineHelper.register(
-				messageListener, schedulerEntry,
-				DestinationNames.SCHEDULER_DISPATCH);
+			SchedulerEventMessageListener schedulerEventMessageListener =
+				new AbortedMultipartUploadMessageListener(
+					_bundleContext.getService(serviceReference),
+					schedulerEntry);
 
-			return messageListener;
+			Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+			properties.put(
+				"destination.name", DestinationNames.SCHEDULER_DISPATCH);
+
+			_serviceRegistration = _bundleContext.registerService(
+				SchedulerEventMessageListener.class,
+				schedulerEventMessageListener, properties);
+
+			return schedulerEventMessageListener;
 		}
 
 		@Override
@@ -154,21 +169,19 @@ public class AbortedMultipartUploadCleaner {
 			ServiceReference<S3Store> serviceReference,
 			MessageListener messageListener) {
 
-			_schedulerEngineHelper.unregister(messageListener);
+			_serviceRegistration.unregister();
 		}
 
 		private S3StoreServiceTrackerCustomizer(
-			BundleContext bundleContext,
-			SchedulerEngineHelper schedulerEngineHelper,
-			TriggerFactory triggerFactory) {
+			BundleContext bundleContext, TriggerFactory triggerFactory) {
 
 			_bundleContext = bundleContext;
-			_schedulerEngineHelper = schedulerEngineHelper;
 			_triggerFactory = triggerFactory;
 		}
 
 		private final BundleContext _bundleContext;
-		private final SchedulerEngineHelper _schedulerEngineHelper;
+		private ServiceRegistration<SchedulerEventMessageListener>
+			_serviceRegistration;
 		private final TriggerFactory _triggerFactory;
 
 	}
