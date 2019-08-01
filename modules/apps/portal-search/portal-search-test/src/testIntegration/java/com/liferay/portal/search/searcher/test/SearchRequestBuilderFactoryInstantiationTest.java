7c13cbac5111dfea598358c696dea66bd34c51bc
hai.yu <8219754@liferay.com>
2019-08-01 12:00:23 +0800

    LPS-99119 run test

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/searcher/test/SearchRequestBuilderFactoryInstantiationTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/searcher/test/SearchRequestBuilderFactoryInstantiationTest.java
@@ -43,6 +43,8 @@ public class SearchRequestBuilderFactoryInstantiationTest {
 		SearchRequestBuilder searchRequestBuilder =
 			_searchRequestBuilderFactory.builder();
 
+		
+		
 		Assert.assertNotNull(searchRequestBuilder);
 	}
 
