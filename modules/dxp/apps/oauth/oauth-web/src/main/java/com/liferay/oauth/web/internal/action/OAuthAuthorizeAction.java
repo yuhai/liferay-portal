8bb94f270bb0c972a1e5eb87533db11833f728a7
hai.yu <8219754@liferay.com>
2019-11-07 14:24:12 +0800

    LPS-104241 Remove deprecated class com.liferay.portal.kernel.struts.BaseStrutsAction with replacements.

--- modules/dxp/apps/oauth/oauth-web/src/main/java/com/liferay/oauth/web/internal/action/OAuthAuthorizeAction.java
+++ modules/dxp/apps/oauth/oauth-web/src/main/java/com/liferay/oauth/web/internal/action/OAuthAuthorizeAction.java
@@ -21,7 +21,6 @@ import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.WindowStateFactory;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.struts.BaseStrutsAction;
 import com.liferay.portal.kernel.struts.StrutsAction;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Http;
@@ -53,7 +52,7 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true, property = "path=" + OAuthConstants.PUBLIC_PATH_AUTHORIZE,
 	service = StrutsAction.class
 )
-public class OAuthAuthorizeAction extends BaseStrutsAction {
+public class OAuthAuthorizeAction implements StrutsAction {
 
 	@Override
 	public String execute(
