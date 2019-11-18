9452da8f6a869ddad85e73e35d59909d95cc8d2b
hai.yu <8219754@liferay.com>
2019-11-18 18:00:23 +0800

    LPS-104504 PortalExecutorManager

--- modules/apps/portal/portal-messaging/src/main/java/com/liferay/portal/messaging/internal/BaseAsyncDestination.java
+++ modules/apps/portal/portal-messaging/src/main/java/com/liferay/portal/messaging/internal/BaseAsyncDestination.java
@@ -14,12 +14,14 @@
 
 package com.liferay.portal.messaging.internal;
 
+import com.liferay.petra.concurrent.NoticeableExecutorService;
+import com.liferay.petra.concurrent.NoticeableThreadPoolExecutor;
+import com.liferay.petra.concurrent.ThreadPoolHandlerAdapter;
+import com.liferay.petra.executor.PortalExecutorManager;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.cluster.ClusterInvokeThreadLocal;
 import com.liferay.portal.kernel.concurrent.RejectedExecutionHandler;
 import com.liferay.portal.kernel.concurrent.ThreadPoolExecutor;
-import com.liferay.portal.kernel.concurrent.ThreadPoolHandlerAdapter;
-import com.liferay.portal.kernel.executor.PortalExecutorManager;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseDestination;
@@ -42,6 +44,7 @@ import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Locale;
 import java.util.Set;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -52,15 +55,17 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 
 	@Override
 	public void close(boolean force) {
-		if ((_threadPoolExecutor == null) || _threadPoolExecutor.isShutdown()) {
+		if ((_noticeableThreadPoolExecutor == null) ||
+			_noticeableThreadPoolExecutor.isShutdown()) {
+
 			return;
 		}
 
 		if (force) {
-			_threadPoolExecutor.shutdownNow();
+			_noticeableThreadPoolExecutor.shutdownNow();
 		}
 		else {
-			_threadPoolExecutor.shutdown();
+			_noticeableThreadPoolExecutor.shutdown();
 		}
 	}
 
@@ -70,19 +75,19 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 			new DestinationStatistics();
 
 		destinationStatistics.setActiveThreadCount(
-			_threadPoolExecutor.getActiveCount());
+			_noticeableThreadPoolExecutor.getActiveCount());
 		destinationStatistics.setCurrentThreadCount(
-			_threadPoolExecutor.getPoolSize());
+			_noticeableThreadPoolExecutor.getPoolSize());
 		destinationStatistics.setLargestThreadCount(
-			_threadPoolExecutor.getLargestPoolSize());
+			_noticeableThreadPoolExecutor.getLargestPoolSize());
 		destinationStatistics.setMaxThreadPoolSize(
-			_threadPoolExecutor.getMaxPoolSize());
+			_noticeableThreadPoolExecutor.getMaximumPoolSize());
 		destinationStatistics.setMinThreadPoolSize(
-			_threadPoolExecutor.getCorePoolSize());
+			_noticeableThreadPoolExecutor.getCorePoolSize());
 		destinationStatistics.setPendingMessageCount(
-			_threadPoolExecutor.getPendingTaskCount());
+			_noticeableThreadPoolExecutor.getPendingTaskCount());
 		destinationStatistics.setSentMessageCount(
-			_threadPoolExecutor.getCompletedTaskCount());
+			_noticeableThreadPoolExecutor.getCompletedTaskCount());
 
 		return destinationStatistics;
 	}
@@ -101,8 +106,8 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 
 	@Override
 	public void open() {
-		if ((_threadPoolExecutor != null) &&
-			!_threadPoolExecutor.isShutdown()) {
+		if ((_noticeableThreadPoolExecutor != null) &&
+			!_noticeableThreadPoolExecutor.isShutdown()) {
 
 			return;
 		}
@@ -113,30 +118,33 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 			_rejectedExecutionHandler = _createRejectionExecutionHandler();
 		}
 
-		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
-			_workersCoreSize, _workersMaxSize, 60L, TimeUnit.SECONDS, false,
-			_maximumQueueSize, _rejectedExecutionHandler,
-			new NamedThreadFactory(
-				getName(), Thread.NORM_PRIORITY, classLoader),
-			new ThreadPoolHandlerAdapter());
+		NoticeableThreadPoolExecutor noticeableThreadPoolExecutor =
+			new NoticeableThreadPoolExecutor(
+				_workersCoreSize, _workersMaxSize, 60L, TimeUnit.SECONDS,
+				new LinkedBlockingQueue<>(_maximumQueueSize),
+				new NamedThreadFactory(
+					getName(), Thread.NORM_PRIORITY, classLoader),
+				new java.util.concurrent.ThreadPoolExecutor.AbortPolicy(),
+				new ThreadPoolHandlerAdapter());
 
-		ThreadPoolExecutor oldThreadPoolExecutor =
+		NoticeableExecutorService oldNoticeableExecutorService =
 			_portalExecutorManager.registerPortalExecutor(
-				getName(), threadPoolExecutor);
+				getName(), noticeableThreadPoolExecutor);
 
-		if (oldThreadPoolExecutor != null) {
+		if (oldNoticeableExecutorService != null) {
 			if (_log.isWarnEnabled()) {
 				_log.warn(
 					"Abort creating a new thread pool for destination " +
 						getName() + " and reuse previous one");
 			}
 
-			threadPoolExecutor.shutdownNow();
+			noticeableThreadPoolExecutor.shutdownNow();
 
-			threadPoolExecutor = oldThreadPoolExecutor;
+			noticeableThreadPoolExecutor =
+				(NoticeableThreadPoolExecutor)oldNoticeableExecutorService;
 		}
 
-		_threadPoolExecutor = threadPoolExecutor;
+		_noticeableThreadPoolExecutor = noticeableThreadPoolExecutor;
 	}
 
 	@Override
@@ -149,9 +157,10 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 			return;
 		}
 
-		ThreadPoolExecutor threadPoolExecutor = _threadPoolExecutor;
+		NoticeableThreadPoolExecutor noticeableThreadPoolExecutor =
+			_noticeableThreadPoolExecutor;
 
-		if (threadPoolExecutor.isShutdown()) {
+		if (noticeableThreadPoolExecutor.isShutdown()) {
 			throw new IllegalStateException(
 				StringBundler.concat(
 					"Destination ", getName(), " is shutdown and cannot ",
@@ -193,30 +202,24 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 	public void setWorkersCoreSize(int workersCoreSize) {
 		_workersCoreSize = workersCoreSize;
 
-		if (_threadPoolExecutor != null) {
-			_threadPoolExecutor.adjustPoolSize(
-				workersCoreSize, _workersMaxSize);
+		if (_noticeableThreadPoolExecutor != null) {
+			_noticeableThreadPoolExecutor.setCorePoolSize(_workersMaxSize);
 		}
 	}
 
 	public void setWorkersMaxSize(int workersMaxSize) {
 		_workersMaxSize = workersMaxSize;
 
-		if (_threadPoolExecutor != null) {
-			_threadPoolExecutor.adjustPoolSize(
-				_workersCoreSize, workersMaxSize);
+		if (_noticeableThreadPoolExecutor != null) {
+			_noticeableThreadPoolExecutor.setMaximumPoolSize(workersMaxSize);
 		}
 	}
 
 	protected abstract void dispatch(
 		Set<MessageListener> messageListeners, Message message);
 
-	/**
-	 * @deprecated As of Judson (7.1.x), with no direct replacement
-	 */
-	@Deprecated
-	protected ThreadPoolExecutor getThreadPoolExecutor() {
-		return _threadPoolExecutor;
+	protected NoticeableThreadPoolExecutor getNoticeableThreadPoolExecutor() {
+		return _noticeableThreadPoolExecutor;
 	}
 
 	protected void populateMessageFromThreadLocals(Message message) {
@@ -363,9 +366,9 @@ public abstract class BaseAsyncDestination extends BaseDestination {
 		BaseAsyncDestination.class);
 
 	private int _maximumQueueSize = Integer.MAX_VALUE;
+	private NoticeableThreadPoolExecutor _noticeableThreadPoolExecutor;
 	private PortalExecutorManager _portalExecutorManager;
 	private RejectedExecutionHandler _rejectedExecutionHandler;
-	private ThreadPoolExecutor _threadPoolExecutor;
 	private int _workersCoreSize = _WORKERS_CORE_SIZE;
 	private int _workersMaxSize = _WORKERS_MAX_SIZE;
 
