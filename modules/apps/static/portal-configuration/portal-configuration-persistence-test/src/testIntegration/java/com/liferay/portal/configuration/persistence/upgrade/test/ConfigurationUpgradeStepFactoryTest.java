56d72f1ec517dd6f655c8fd1da6b3db28cf7ac23
hai.yu <8219754@liferay.com>
2020-01-15 14:07:53 +0800

    LPS-89497 Add integration test for ConfigurationUpgradeStepFactory.

---
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-test/src/testIntegration/java/com/liferay/portal/configuration/persistence/upgrade/test/ConfigurationUpgradeStepFactoryTest.java
@@ -0,0 +1,333 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configuration.persistence.upgrade.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.persistence.upgrade.ConfigurationUpgradeStepFactory;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
+import com.liferay.portal.kernel.dao.db.DBContext;
+import com.liferay.portal.kernel.dao.db.DBProcessContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.util.PropsValues;
+
+import java.io.File;
+import java.io.OutputStream;
+
+import java.net.URI;
+
+import org.apache.felix.cm.PersistenceManager;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Hai Yu
+ */
+@RunWith(Arquillian.class)
+public class ConfigurationUpgradeStepFactoryTest {
+
+	@ClassRule
+	@Rule
+	public static AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testCreateUpgradeStepWithConfigFileExistAndDataNotExist()
+		throws Exception {
+
+		_assertConfigFile(
+			new File(
+				PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR, _OLD_PID + ".config"),
+			new File(
+				PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR, _NEW_PID + ".config"),
+			_OLD_PID, _NEW_PID);
+	}
+
+	@Test
+	public void testCreateUpgradeStepWithConfigFileExitAndDataExit()
+		throws Exception {
+
+		File oldConfigFile = new File(
+			PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR, _OLD_PID + ".config");
+
+		oldConfigFile.createNewFile();
+
+		try {
+			_assertConfig(
+				oldConfigFile,
+				new File(
+					PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
+					_NEW_PID + ".config"));
+		}
+		finally {
+			oldConfigFile.delete();
+		}
+	}
+
+	@Test
+	public void testCreateUpgradeStepWithConfigFileNotExitAndDataExit()
+		throws Exception {
+
+		_assertConfig(null, null);
+	}
+
+	@Test
+	public void testCreateUpgradeStepWithFactoryConfigFileExistAndDataExist()
+		throws Exception {
+
+		Configuration configuration =
+			_configurationAdmin.createFactoryConfiguration(
+				_OLD_PID, StringPool.QUESTION);
+
+		String oldPid = configuration.getPid();
+
+		String newPid = StringUtil.replace(oldPid, _OLD_PID, _NEW_PID);
+
+		File oldConfigFile = new File(
+			PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR, oldPid + ".config");
+
+		try {
+			oldConfigFile.createNewFile();
+
+			_assertConfigFactory(
+				oldConfigFile,
+				new File(
+					PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
+					newPid + ".config"),
+				oldPid, newPid, configuration);
+		}
+		finally {
+			ConfigurationTestUtil.deleteConfiguration(configuration);
+		}
+	}
+
+	@Test
+	public void testCreateUpgradeStepWithFactoryConfigFileExistAndDataNotExist()
+		throws Exception {
+
+		_assertConfigFile(
+			new File(
+				PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
+				_OLD_PID + "-instance.config"),
+			new File(
+				PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
+				_NEW_PID + "-instance.config"),
+			_OLD_PID + "-instance", _NEW_PID + "-instance");
+	}
+
+	@Test
+	public void testCreateUpgradeStepWithFactoryConfigFileNotExistAndDataExist()
+		throws Exception {
+
+		Configuration configuration =
+			_configurationAdmin.createFactoryConfiguration(
+				_OLD_PID, StringPool.QUESTION);
+
+		String oldPid = configuration.getPid();
+
+		String newPid = StringUtil.replace(oldPid, _OLD_PID, _NEW_PID);
+
+		try {
+			_assertConfigFactory(
+				new File(
+					PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
+					oldPid + ".config"),
+				null, oldPid, newPid, configuration);
+		}
+		finally {
+			ConfigurationTestUtil.deleteConfiguration(configuration);
+		}
+	}
+
+	private void _assertConfig(File oldConfigFile, File newConfigFile)
+		throws Exception {
+
+		Configuration configuration = _configurationAdmin.getConfiguration(
+			_OLD_PID);
+
+		try {
+			Assert.assertTrue(
+				"Configuration " + _OLD_PID + " does not exist",
+				_persistenceManager.exists(_OLD_PID));
+
+			if (oldConfigFile != null) {
+				Assert.assertTrue(
+					"Configuration file " + _OLD_PID + ".config does not exist",
+					oldConfigFile.exists());
+			}
+
+			UpgradeStep upgradeStep =
+				_configurationUpgradeStepFactory.createUpgradeStep(
+					_OLD_PID, _NEW_PID);
+
+			upgradeStep.upgrade(_DB_PROCESS_CONTEXT);
+
+			Assert.assertFalse(
+				"Configuration " + _OLD_PID + " still exists",
+				_persistenceManager.exists(_OLD_PID));
+			Assert.assertTrue(
+				"Configuration " + _NEW_PID + " does not exist",
+				_persistenceManager.exists(_NEW_PID));
+
+			if (oldConfigFile != null) {
+				Assert.assertFalse(
+					"Configuration file " + _OLD_PID + " .config still exists",
+					oldConfigFile.exists());
+				Assert.assertTrue(
+					"Configuration file " + _NEW_PID + " does not exist",
+					newConfigFile.exists());
+			}
+		}
+		finally {
+			if (oldConfigFile != null) {
+				oldConfigFile.delete();
+				newConfigFile.delete();
+			}
+
+			_persistenceManager.delete(_OLD_PID);
+			_persistenceManager.delete(_NEW_PID);
+
+			ConfigurationTestUtil.deleteConfiguration(configuration);
+		}
+	}
+
+	private void _assertConfigFactory(
+			File oldConfigFile, File newConfigFile, String oldPid,
+			String newPid, Configuration configuration)
+		throws Exception {
+
+		URI uri = oldConfigFile.toURI();
+
+		try {
+			ConfigurationTestUtil.saveConfiguration(
+				configuration,
+				MapUtil.singletonDictionary(
+					"felix.fileinstall.filename", uri.toString()));
+
+			Assert.assertTrue(
+				"Configuration " + oldPid + " does not exist",
+				_persistenceManager.exists(oldPid));
+
+			if (newConfigFile != null) {
+				Assert.assertTrue(
+					"Configuration file " + oldPid + ".config does not exist",
+					oldConfigFile.exists());
+			}
+
+			UpgradeStep upgradeStep =
+				_configurationUpgradeStepFactory.createUpgradeStep(
+					_OLD_PID, _NEW_PID);
+
+			upgradeStep.upgrade(_DB_PROCESS_CONTEXT);
+
+			Assert.assertFalse(
+				"Configuration " + oldPid + " still exists",
+				_persistenceManager.exists(oldPid));
+			Assert.assertTrue(
+				"Configuration " + newPid + " does not exist",
+				_persistenceManager.exists(newPid));
+			Assert.assertFalse(
+				"Configuration file " + oldPid + " .config still exists",
+				oldConfigFile.exists());
+
+			if (newConfigFile != null) {
+				Assert.assertTrue(
+					"Configuration file " + newPid + " does not exist",
+					newConfigFile.exists());
+			}
+		}
+		finally {
+			if (newConfigFile != null) {
+				oldConfigFile.delete();
+				newConfigFile.delete();
+			}
+
+			_persistenceManager.delete(oldPid);
+			_persistenceManager.delete(newPid);
+		}
+	}
+
+	private void _assertConfigFile(
+			File oldConfigFile, File newConfigFile, String oldPid,
+			String newPid)
+		throws Exception {
+
+		try {
+			oldConfigFile.createNewFile();
+
+			Assert.assertTrue(
+				"Configuration file " + oldPid + ".config does not exist",
+				oldConfigFile.exists());
+
+			UpgradeStep upgradeStep =
+				_configurationUpgradeStepFactory.createUpgradeStep(
+					oldPid, newPid);
+
+			upgradeStep.upgrade(_DB_PROCESS_CONTEXT);
+
+			Assert.assertFalse(
+				"Configuration file " + oldPid + " .config still exists",
+				oldConfigFile.exists());
+			Assert.assertTrue(
+				"Configuration file " + newPid + " does not exist",
+				newConfigFile.exists());
+		}
+		finally {
+			oldConfigFile.delete();
+			newConfigFile.delete();
+		}
+	}
+
+	private static final DBProcessContext _DB_PROCESS_CONTEXT =
+		new DBProcessContext() {
+
+			@Override
+			public DBContext getDBContext() {
+				return new DBContext();
+			}
+
+			@Override
+			public OutputStream getOutputStream() {
+				return null;
+			}
+
+		};
+
+	private static final String _NEW_PID = "test.new.pid";
+
+	private static final String _OLD_PID = "test.old.pid";
+
+	@Inject
+	private ConfigurationAdmin _configurationAdmin;
+
+	@Inject
+	private ConfigurationUpgradeStepFactory _configurationUpgradeStepFactory;
+
+	@Inject
+	private PersistenceManager _persistenceManager;
+
+}
