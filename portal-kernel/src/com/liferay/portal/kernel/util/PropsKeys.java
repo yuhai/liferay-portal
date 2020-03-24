c5294277dfc3bd11dc59c226e1689b1f84a0301f
hai.yu <8219754@liferay.com>
2020-03-24 09:35:03 +0800

    LPS-110758 Remove the rest properties of portal resiliency

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2492,9 +2492,17 @@ public interface PropsKeys {
 	public static final String PORTAL_RESILIENCY_ENABLED =
 		"portal.resiliency.enabled";
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String PORTAL_RESILIENCY_PORTLET_SHOW_FOOTER =
 		"portal.resiliency.portlet.show.footer";
 
+	/**
+	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
+	 */
+	@Deprecated
 	public static final String
 		PORTAL_RESILIENCY_SPI_AGENT_CLIENT_POOL_MAX_SIZE =
 			"portal.resiliency.spi.agent.client.pool.max.size";
