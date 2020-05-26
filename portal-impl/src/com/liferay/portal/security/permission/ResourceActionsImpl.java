870923c940e120daf73f3ff16568532af41f9d38
Dante Wang <dante.wang@liferay.com>
2020-05-26 16:37:16 +0800

    LPS-121111 When the ResourceActionsBag is newly created (not previously read from file), the method tries to populate the bag with values. However, the values are not added to the portlet actions list, causing difference between the values stored in DB and the in-memory resource actions bag.

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -965,7 +965,7 @@ public class ResourceActionsImpl implements ResourceActions {
 		}
 
 		synchronized (this) {
-			portletActions = _getPortletMimeTypeActions(name, portlet);
+			portletActions.addAll(_getPortletMimeTypeActions(name, portlet));
 
 			_checkPortletActions(portlet, portletActions);
 
