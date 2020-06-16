c2ae105bb4c29f1be30bc8731fa5ade032fcb46a
Dante Wang <dante.wang@liferay.com>
2020-06-16 16:50:00 +0800

    LPS-121111 SF, inline

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -814,18 +814,6 @@ public class ResourceActionsImpl implements ResourceActions {
 		}
 	}
 
-	private void _checkPortletGroupDefaultActions(Set<String> actions) {
-		if (actions.isEmpty()) {
-			actions.add(ActionKeys.VIEW);
-		}
-	}
-
-	private void _checkPortletGuestDefaultActions(Set<String> actions) {
-		if (actions.isEmpty()) {
-			actions.add(ActionKeys.VIEW);
-		}
-	}
-
 	private void _checkPortletLayoutManagerActions(Set<String> actions) {
 		if (!actions.contains(ActionKeys.ACCESS_IN_CONTROL_PANEL) &&
 			!actions.contains(ActionKeys.ADD_TO_PAGE)) {
@@ -942,11 +930,19 @@ public class ResourceActionsImpl implements ResourceActions {
 
 			_checkPortletActions(portlet, portletActions);
 
-			_checkPortletGroupDefaultActions(
-				resourceActionsBag.getGroupDefaultActions());
+			Set<String> groupDefaultActions =
+				resourceActionsBag.getGroupDefaultActions();
 
-			_checkPortletGuestDefaultActions(
-				resourceActionsBag.getGuestDefaultActions());
+			if (groupDefaultActions.isEmpty()) {
+				groupDefaultActions.add(ActionKeys.VIEW);
+			}
+
+			Set<String> guestDefaultActions =
+				resourceActionsBag.getGuestDefaultActions();
+
+			if (guestDefaultActions.isEmpty()) {
+				guestDefaultActions.add(ActionKeys.VIEW);
+			}
 
 			_checkPortletLayoutManagerActions(
 				resourceActionsBag.getLayoutManagerActions());
