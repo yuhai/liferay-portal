4b27c956432713aebf93e646a509644e9f65ce26
Dante Wang <dante.wang@liferay.com>
2020-06-16 15:57:52 +0800

    LPS-121111 Remove redundant check, this logic won't run for portlet "90"

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -967,9 +967,7 @@ public class ResourceActionsImpl implements ResourceActions {
 		synchronized (this) {
 			portletActions = _getPortletMimeTypeActions(name, portlet);
 
-			if (!name.equals(PortletKeys.PORTAL)) {
-				_checkPortletActions(portlet, portletActions);
-			}
+			_checkPortletActions(portlet, portletActions);
 
 			_checkPortletGroupDefaultActions(
 				portletResourceActionsBag.getGroupDefaultActions());
