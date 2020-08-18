70820ff9cf95ab3e5efd3c05b48b01a66f7a1d27
hai.yu <8219754@liferay.com>
2020-08-18 11:12:55 +0800

    LPS-117531 SF, Extract common logic to _validatePublicId()

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -649,17 +649,7 @@ public class ResourceActionsImpl implements ResourceActions {
 			Set<String> resourceNames)
 		throws ResourceActionsException {
 
-		DocumentType documentType = document.getDocumentType();
-
-		String publicId = GetterUtil.getString(documentType.getPublicId());
-
-		if (publicId.equals(
-				"-//Liferay//DTD Resource Action Mapping 6.0.0//EN")) {
-
-			if (_log.isWarnEnabled()) {
-				_log.warn("Please update document to use the 6.1.0 format");
-			}
-		}
+		_validatePublicId(document);
 
 		_readModelResource(
 			servletContextName, document.getRootElement(), resourceNames);
@@ -1096,18 +1086,7 @@ public class ResourceActionsImpl implements ResourceActions {
 		try {
 			Document document = UnsecureSAXReaderUtil.read(inputStream, true);
 
-			DocumentType documentType = document.getDocumentType();
-
-			String publicId = GetterUtil.getString(documentType.getPublicId());
-
-			if (publicId.equals(
-					"-//Liferay//DTD Resource Action Mapping 6.0.0//EN")) {
-
-				if (_log.isWarnEnabled()) {
-					_log.warn(
-						"Please update " + source + " to use the 6.1.0 format");
-				}
-			}
+			_validatePublicId(document);
 
 			Element rootElement = document.getRootElement();
 
@@ -1366,6 +1345,20 @@ public class ResourceActionsImpl implements ResourceActions {
 		layoutManagerActions.clear();
 
 		_readActionKeys(layoutManagerActions, layoutManagerElement);
+	}
+
+	private void _validatePublicId(Document document) {
+		DocumentType documentType = document.getDocumentType();
+
+		String publicId = GetterUtil.getString(documentType.getPublicId());
+
+		if (publicId.equals(
+				"-//Liferay//DTD Resource Action Mapping 6.0.0//EN")) {
+
+			if (_log.isWarnEnabled()) {
+				_log.warn("Please update document to use the 6.1.0 format");
+			}
+		}
 	}
 
 	private static final String _ACTION_NAME_PREFIX = "action.";
