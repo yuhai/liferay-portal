19e481d0f0bfdff77e08e6043714f30284f27966
hai.yu <8219754@liferay.com>
2020-08-20 16:10:56 +0800

    LPS-118542 Add integration test for module-log4j configuration

---
+++ modules/apps/portal/portal-log4j-test/src/testIntegration/java/com/liferay/portal/log4j/test/Log4jTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.log4j.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.log4j.Log4JUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Hai Yu
+ */
+@RunWith(Arquillian.class)
+public class Log4jTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@After
+	public void tearDown() {
+		Log4JUtil.setLevel(
+			"com.liferay.portal.kernel.util.ServerDetector", "INFO", false);
+	}
+
+	@Test
+	public void testLogger() {
+		Assert.assertTrue(
+			"The logger Log4jTest level should be WARN",
+			_log.isWarnEnabled() && !_log.isInfoEnabled());
+
+		String level = Log4JUtil.getOriginalLevel(
+			"com.liferay.portal.kernel.util.ServerDetector");
+
+		Assert.assertEquals(
+			"The original level should be DEBUG", "DEBUG", level);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(Log4jTest.class);
+
+}
