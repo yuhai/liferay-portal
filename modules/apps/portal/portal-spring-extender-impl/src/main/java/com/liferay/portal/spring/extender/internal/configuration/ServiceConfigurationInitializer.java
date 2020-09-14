6a30b85233c4bd78e5b1ce2b5be0eafc9b89ca7e
hai.yu <8219754@liferay.com>
2020-09-14 18:29:35 +0800

    LPS-117531 Apply to ServiceConfigurationInitializer, only read model resources from service modules

--- modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/configuration/ServiceConfigurationInitializer.java
+++ modules/apps/portal/portal-spring-extender-impl/src/main/java/com/liferay/portal/spring/extender/internal/configuration/ServiceConfigurationInitializer.java
@@ -130,14 +130,15 @@ public class ServiceConfigurationInitializer {
 				"service.configurator.portlet.ids");
 
 			if (Validator.isNull(portlets)) {
-				_resourceActions.readAndCheck(
-					null, _classLoader,
-					StringUtil.split(
-						_portletConfiguration.get(
-							PropsKeys.RESOURCE_ACTIONS_CONFIGS)));
+				_resourceActions.checkResourceActions(
+					_resourceActions.readModelResource(
+						null, _classLoader,
+						StringUtil.split(
+							_portletConfiguration.get(
+								PropsKeys.RESOURCE_ACTIONS_CONFIGS))));
 			}
 			else {
-				_resourceActions.read(
+				_resourceActions.readModelResource(
 					null, _classLoader,
 					StringUtil.split(
 						_portletConfiguration.get(
