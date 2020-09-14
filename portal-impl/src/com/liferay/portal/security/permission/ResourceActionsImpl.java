ab1e45aea3487f12e1f64a0c3b8e24d6b67688b4
Dante Wang <dante.wang@liferay.com>
2020-09-14 20:55:27 +0800

    LPS-117531 Honor actions defined in xml files, don't call _checkPortletActions() which auto adds ACCESS_IN_CONTROL_PANEL; before change this won't be added for portlet resources read from file because portlet is null.

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -1257,7 +1257,7 @@ public class ResourceActionsImpl implements ResourceActions {
 					portletName, portlet);
 
 				if (!portletName.equals(PortletKeys.PORTAL)) {
-					_checkPortletActions(portlet, portletActions);
+					_checkPortletLayoutManagerActions(portletActions);
 				}
 
 				_readResource(
