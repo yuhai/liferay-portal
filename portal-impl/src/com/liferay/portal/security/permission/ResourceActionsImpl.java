81078944d510294864381ec39a2bbb5798fabb35
hai.yu <8219754@liferay.com>
2020-09-18 21:49:55 +0800

    LPS-121111 SF, remove method's param.

--- portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
+++ portal-impl/src/com/liferay/portal/security/permission/ResourceActionsImpl.java
@@ -1221,8 +1221,7 @@ public class ResourceActionsImpl implements ResourceActions {
 
 			_readResource(
 				modelResourceElement, modelName,
-				Collections.singleton(ActionKeys.PERMISSIONS),
-				Collections.emptySet());
+				Collections.singleton(ActionKeys.PERMISSIONS));
 
 			if (modelNames != null) {
 				modelNames.add(modelName);
@@ -1263,8 +1262,7 @@ public class ResourceActionsImpl implements ResourceActions {
 				}
 
 				_readResource(
-					portletResourceElement, portletName, portletActions,
-					_defaultPortletGuestUnsupportedActions);
+					portletResourceElement, portletName, portletActions);
 
 				if (portletNames != null) {
 					portletNames.add(portletName);
@@ -1275,8 +1273,7 @@ public class ResourceActionsImpl implements ResourceActions {
 
 	private void _readResource(
 			Element resourceElement, String name,
-			Set<String> defaultResourceActions,
-			Set<String> defaultGuestUnsupportedActions)
+			Set<String> defaultResourceActions)
 		throws ResourceActionsException {
 
 		ResourceActionsBag resourceActionsBag = _getResourceActionsBag(name);
@@ -1333,7 +1330,12 @@ public class ResourceActionsImpl implements ResourceActions {
 		Set<String> guestUnsupportedActions =
 			resourceActionsBag.getGuestUnsupportedActions();
 
-		guestUnsupportedActions.addAll(defaultGuestUnsupportedActions);
+		String resourceElementName = resourceElement.getName();
+
+		if (resourceElementName.equals("portlet-resource")) {
+			guestUnsupportedActions.addAll(
+				_defaultPortletGuestUnsupportedActions);
+		}
 
 		Element guestUnsupportedElement = _getPermissionsChildElement(
 			resourceElement, "guest-unsupported");
