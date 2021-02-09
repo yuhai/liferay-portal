8cf22450b40595066803130e4d89dcec823c7af5
hai.yu <8219754@liferay.com>
2021-02-09 15:18:59 +0800

    LPS-125550 Apply log4j2 api.

--- modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
+++ modules/apps/petra/petra-log4j/src/main/java/com/liferay/petra/log4j/Log4JUtil.java
@@ -39,7 +39,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
-import org.apache.log4j.Level;
+import org.apache.logging.log4j.Level;
 
 /**
  * @author Brian Wing Shun Chan
