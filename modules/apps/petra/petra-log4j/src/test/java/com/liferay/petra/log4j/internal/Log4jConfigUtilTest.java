0c1b16b09c11a0fd205555f138ce440dbe82c75e
hai.yu <8219754@liferay.com>
2021-02-23 23:50:22 +0800

    LPS-125550 Unit Test, add the childElement to cover _removeAppender method of Log4jConfigUtil (the code Objects.equals("AppenderRef", childElement.getName())). In log4j1 test, <priority> covered the code.

--- modules/apps/petra/petra-log4j/src/test/java/com/liferay/petra/log4j/internal/Log4jConfigUtilTest.java
+++ modules/apps/petra/petra-log4j/src/test/java/com/liferay/petra/log4j/internal/Log4jConfigUtilTest.java
@@ -329,7 +329,7 @@ public class Log4jConfigUtilTest {
 				if (appenderType.equals(ConsoleAppender.class)) {
 					sb.append("<Console name=\"");
 					sb.append(appenderType.getName());
-					sb.append("\" />");
+					sb.append("\"><PatternLayout /></Console>");
 				}
 				else {
 					sb.append("<Null name=\"");
