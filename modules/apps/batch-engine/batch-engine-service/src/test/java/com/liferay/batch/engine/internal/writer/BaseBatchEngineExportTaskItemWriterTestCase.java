b41f925d0d80de1d3b0f3b283182d025ff31d059
hai.yu <8219754@liferay.com>
2021-03-31 14:26:30 +0800

    LPS-129172 In jackson-databind-2.10.3, due to the filed _tzSerializedWithColon default vale of com.fasterxml.jackson.databind.util.StdDateFormat was false, it will append "+0000" to the end. In jackson-databind-2.12.2, the field default value is true, so it appended "+00:00"(use iso8601Pattern) to the end when serialize the date. Our format "SSSZ" can match "+0000". In java.text.SimpleDateFormat, when format 'X'(for the fix), it will append 'Z' to the end, instead of "+/-00:00", please refer to subFormat(){case PATTERN_ISO_ZONE:   // 'X' }.

--- modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/writer/BaseBatchEngineExportTaskItemWriterTestCase.java
+++ modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/writer/BaseBatchEngineExportTaskItemWriterTestCase.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonFilter;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.lang.reflect.Field;
 
@@ -206,7 +207,7 @@ public abstract class BaseBatchEngineExportTaskItemWriterTestCase {
 	protected static final List<String> columnFieldNames = Arrays.asList(
 		"createDate", "description", "id", "name_en", "name_hr");
 	protected static final DateFormat dateFormat = new SimpleDateFormat(
-		"yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		"yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 	protected static Map<String, Field> fieldMap = ItemClassIndexUtil.index(
 		Item.class);
 	protected static final List<String> jsonFieldNames = Arrays.asList(
@@ -218,7 +219,9 @@ public abstract class BaseBatchEngineExportTaskItemWriterTestCase {
 		}
 
 		if (value instanceof Date) {
-			return "\"" + dateFormat.format(value) + "\"";
+			return "\"" +
+				StringUtil.replace(dateFormat.format(value), 'Z', "+00:00") +
+					"\"";
 		}
 
 		if (value instanceof String) {
