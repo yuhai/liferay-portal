209db199f439e96db36d21e40dbdfbccb7a57484
Dante Wang <dante.wang@liferay.com>
2021-05-13 19:15:55 +0800

    LPS-X Migrate SessionImpl: 1) Session#close() in Hibernate 5 no longer returns Connection, return null here to quick fail; 2) SessionFactoryImplementor#getEntityPersister(String) -> MetamodelImplementor#entityPersister(Class); 3) constructor of EntityKey

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/SessionImpl.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/SessionImpl.java
@@ -34,10 +34,12 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.hibernate.LockOptions;
-import org.hibernate.engine.EntityKey;
-import org.hibernate.engine.PersistenceContext;
-import org.hibernate.engine.SessionFactoryImplementor;
-import org.hibernate.event.EventSource;
+import org.hibernate.engine.spi.EntityKey;
+import org.hibernate.engine.spi.PersistenceContext;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.event.spi.EventSource;
+import org.hibernate.metamodel.spi.MetamodelImplementor;
+import org.hibernate.persister.entity.EntityPersister;
 
 /**
  * @author Brian Wing Shun Chan
@@ -86,7 +88,9 @@ public class SessionImpl implements Session {
 	@Override
 	public Connection close() throws ORMException {
 		try {
-			return _session.close();
+			_session.close();
+
+			return null;
 		}
 		catch (Exception exception) {
 			throw ExceptionTranslator.translate(exception);
@@ -232,12 +236,14 @@ public class SessionImpl implements Session {
 			SessionFactoryImplementor sessionFactoryImplementor =
 				eventSource.getFactory();
 
+			MetamodelImplementor metamodelImplementor =
+				sessionFactoryImplementor.getMetamodel();
+
+			EntityPersister entityPersister =
+				metamodelImplementor.entityPersister(clazz);
+
 			Object object = persistenceContext.getEntity(
-				new EntityKey(
-					id,
-					sessionFactoryImplementor.getEntityPersister(
-						clazz.getName()),
-					eventSource.getEntityMode()));
+				new EntityKey(id, entityPersister));
 
 			if (object == null) {
 				return;
