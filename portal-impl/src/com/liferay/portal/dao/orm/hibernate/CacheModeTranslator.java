406f36d5e065a4a64051559ce2ad4cb883f096f6
Dante Wang <dante.wang@liferay.com>
2021-05-13 10:27:43 +0800

    LPS-X Migrate CacheModeTranslator: org.hibernate.CacheMode#parse(String) -> org.hibernate.CacheMode#interpretExternalSetting(String)

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/CacheModeTranslator.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/CacheModeTranslator.java
@@ -38,7 +38,8 @@ public class CacheModeTranslator {
 			return org.hibernate.CacheMode.REFRESH;
 		}
 
-		return org.hibernate.CacheMode.parse(cacheMode.getName());
+		return org.hibernate.CacheMode.interpretExternalSetting(
+			cacheMode.getName());
 	}
 
 }
