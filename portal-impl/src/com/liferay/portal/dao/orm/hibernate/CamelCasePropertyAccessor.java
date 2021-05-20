92fd6a53361f2b587c54a9e9c9d676f9bbf3fb7d
Dante Wang <dante.wang@liferay.com>
2021-05-20 17:23:56 +0800

    LPS-X Migrate customized property accessors: PropertyAccessor -> PropertyAccessStrategy that works as factory of PropertyAccess

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/CamelCasePropertyAccessor.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/CamelCasePropertyAccessor.java
@@ -14,29 +14,44 @@
 
 package com.liferay.portal.dao.orm.hibernate;
 
+import org.hibernate.property.access.spi.PropertyAccess;
+
 /**
- * @author Brian Wing Shun Chan
+ * @author Dante Wang
  */
 public class CamelCasePropertyAccessor extends LiferayPropertyAccessor {
 
+	public static final CamelCasePropertyAccessor INSTANCE =
+		new CamelCasePropertyAccessor();
+
 	@Override
-	protected String formatPropertyName(String propertyName) {
-		if (propertyName.length() < 3) {
-			return super.formatPropertyName(propertyName);
-		}
+	public PropertyAccess buildPropertyAccess(
+		Class containerJavaType, String propertyName) {
 
-		char c0 = propertyName.charAt(0);
-		char c1 = propertyName.charAt(1);
-		char c2 = propertyName.charAt(2);
+		return new LiferayPropertyAccess(
+			this, containerJavaType, propertyName) {
 
-		if (Character.isLowerCase(c0) && Character.isUpperCase(c1) &&
-			Character.isLowerCase(c2)) {
+			@Override
+			protected String formatPropertyName(String propertyName) {
+				if (propertyName.length() < 3) {
+					return super.formatPropertyName(propertyName);
+				}
 
-			propertyName =
-				Character.toUpperCase(c0) + propertyName.substring(1);
-		}
+				char c0 = propertyName.charAt(0);
+				char c1 = propertyName.charAt(1);
+				char c2 = propertyName.charAt(2);
 
-		return super.formatPropertyName(propertyName);
+				if (Character.isLowerCase(c0) && Character.isUpperCase(c1) &&
+					Character.isLowerCase(c2)) {
+
+					propertyName =
+						Character.toUpperCase(c0) + propertyName.substring(1);
+				}
+
+				return super.formatPropertyName(propertyName);
+			}
+
+		};
 	}
 
 }
--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/PrivatePropertyAccessor.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/PrivatePropertyAccessor.java
@@ -17,45 +17,35 @@ package com.liferay.portal.dao.orm.hibernate;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.impl.BaseModelImpl;
 
-import org.hibernate.PropertyNotFoundException;
-import org.hibernate.property.DirectPropertyAccessor;
-import org.hibernate.property.Getter;
-import org.hibernate.property.Setter;
+import org.hibernate.property.access.internal.PropertyAccessFieldImpl;
+import org.hibernate.property.access.internal.PropertyAccessStrategyFieldImpl;
+import org.hibernate.property.access.spi.PropertyAccess;
 
 /**
- * @author Shuyang Zhou
+ * @author Dante Wang
  */
 @SuppressWarnings("rawtypes")
-public class PrivatePropertyAccessor extends DirectPropertyAccessor {
+public class PrivatePropertyAccessor extends PropertyAccessStrategyFieldImpl {
+
+	public static final PrivatePropertyAccessor INSTANCE =
+		new PrivatePropertyAccessor();
 
 	@Override
-	public Getter getGetter(Class clazz, String propertyName)
-		throws PropertyNotFoundException {
+	public PropertyAccess buildPropertyAccess(
+		Class containerJavaType, String propertyName) {
 
 		Class<?> superClass = null;
 
-		while ((superClass = clazz.getSuperclass()) != BaseModelImpl.class) {
-			clazz = superClass;
+		while ((superClass = containerJavaType.getSuperclass()) !=
+					BaseModelImpl.class) {
+
+			containerJavaType = superClass;
 		}
 
 		propertyName = StringPool.UNDERLINE.concat(propertyName);
 
-		return super.getGetter(clazz, propertyName);
-	}
-
-	@Override
-	public Setter getSetter(Class clazz, String propertyName)
-		throws PropertyNotFoundException {
-
-		Class<?> superClass = null;
-
-		while ((superClass = clazz.getSuperclass()) != BaseModelImpl.class) {
-			clazz = superClass;
-		}
-
-		propertyName = StringPool.UNDERLINE.concat(propertyName);
-
-		return super.getSetter(clazz, propertyName);
+		return new PropertyAccessFieldImpl(
+			this, containerJavaType, propertyName);
 	}
 
 }
