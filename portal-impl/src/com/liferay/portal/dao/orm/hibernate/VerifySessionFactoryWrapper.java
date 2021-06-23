d0234751a91e2ebecd13a9c5785b51c74ca3bdc0
hai.yu <8219754@liferay.com>
2021-06-23 14:10:43 +0800

    LPS-X Migrate VerifySessionFactoryWrapper: org.hibernate.internal.SessionFactoryImpl#getAllClassMetadata() -> org.hibernate.internal.SessionFactoryImpl#getMetamodel()

--- portal-impl/src/com/liferay/portal/dao/orm/hibernate/VerifySessionFactoryWrapper.java
+++ portal-impl/src/com/liferay/portal/dao/orm/hibernate/VerifySessionFactoryWrapper.java
@@ -33,6 +33,7 @@ import java.lang.reflect.Method;
 import java.sql.Connection;
 
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.metamodel.spi.MetamodelImplementor;
 
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -110,15 +111,20 @@ public class VerifySessionFactoryWrapper implements SessionFactory {
 		SessionFactoryImplementor currentSessionFactoryImplementor,
 		SessionFactoryImplementor targetSessionFactoryImplementor) {
 
+		MetamodelImplementor currentSessionMetamodelImplementor =
+			currentSessionFactoryImplementor.getMetamodel();
+		MetamodelImplementor targetSessionMetamodelImplementor =
+			targetSessionFactoryImplementor.getMetamodel();
+
 		_log.error(
 			"Failed session factory verification",
 			new IllegalStateException(
 				StringBundler.concat(
 					"Wrong current transaction manager, current session ",
 					"factory classes metadata: ",
-					currentSessionFactoryImplementor.getAllClassMetadata(),
+					currentSessionMetamodelImplementor.entityPersisters(),
 					", target session factory classes metadata: ",
-					targetSessionFactoryImplementor.getAllClassMetadata())));
+					targetSessionMetamodelImplementor.entityPersisters())));
 	}
 
 	private boolean _verify() {
