bcd5166335b261949a825ded57614a4c1ac56f31
Dante Wang <dante.wang@liferay.com>
2021-07-01 14:28:59 +0800

    LPS-X Fix exception check in LockLocalServiceTest

--- modules/apps/portal-lock/portal-lock-test/src/testIntegration/java/com/liferay/portal/lock/service/test/LockLocalServiceTest.java
+++ modules/apps/portal-lock/portal-lock-test/src/testIntegration/java/com/liferay/portal/lock/service/test/LockLocalServiceTest.java
@@ -47,6 +47,8 @@ import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
+import javax.persistence.PersistenceException;
+
 import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.hibernate.exception.ConstraintViolationException;
 import org.hibernate.exception.GenericJDBCException;
@@ -273,10 +275,15 @@ public class LockLocalServiceTest {
 			Assert.fail();
 		}
 		catch (ExecutionException executionException) {
-			Throwable throwable = executionException.getCause();
+			Throwable throwable1 = executionException.getCause();
 
 			Assert.assertSame(
-				ConstraintViolationException.class, throwable.getClass());
+				PersistenceException.class, throwable1.getClass());
+
+			Throwable throwable2 = throwable1.getCause();
+
+			Assert.assertSame(
+				ConstraintViolationException.class, throwable2.getClass());
 		}
 	}
 
