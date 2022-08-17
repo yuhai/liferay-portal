df4777d0c994ceb97c78c376d164a0b28e156496
hai.yu <hai.yu@liferay.com>
2022-08-17 16:24:25 +0800

    LPS-portal-service  Move InitialUpgradeExtender to new module.

--- modules/apps/portal/portal-service-extender-impl/src/main/java/com/liferay/portal/service/extender/internal/upgrade/InitialUpgradeExtender.java
+++ modules/apps/portal/portal-service-extender-impl/src/main/java/com/liferay/portal/service/extender/internal/upgrade/InitialUpgradeExtender.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.spring.extender.internal.upgrade;
+package com.liferay.portal.service.extender.internal.upgrade;
 
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -31,7 +31,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.module.util.BundleUtil;
-import com.liferay.portal.spring.extender.internal.upgrade.InitialUpgradeExtender.InitialUpgradeExtension;
+import com.liferay.portal.service.extender.internal.upgrade.InitialUpgradeExtender.InitialUpgradeExtension;
 import com.liferay.portal.spring.hibernate.DialectDetector;
 
 import java.sql.Connection;
